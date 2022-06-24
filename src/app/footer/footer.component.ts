7b4c096160bd99488d48c1295ef7338620191ea8
Ankit Maheshwari <maheshwari.ankit17@gmail.com>
2022-06-24 18:36:29 +0530

    initial source code

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
+})
+
+export class FooterComponent{
+    // constructor will be called at the time of the creation of object.
+    constructor(){
+
+    }
+    // ngOnInit method will be triggered on load of the component...once object has been created for this component then this component will be loaded in this browser and once the loading of the component done then ngOnInit() function called and it can be onload function onload of the page
+    ngOnInit(): void{
+        
+    }
+}
